8d1b179c01c5894b86f4732501d9e542d4c9cb8d
qwerji <bentswanson@gmail.com>
2017-04-11 09:58:21 -0700

    adds bump.js collisions

--- client/scripts/game.js
+++ client/scripts/game.js
@@ -9,13 +9,17 @@ function Game() {
         this.player.update()
 
         // Loop opponents and update
+        // for (var i = 0; i < opponents.length; i++) {
+        //     opponents[i].update()
+        // }
 
+        this.bumpCollisions()
         renderer.render(stage)
     }
 
     this.setup = () => {
-        console.log('Setup')
-
+        console.log('Textures Loaded')
+        
         const player = new Player()
         player.setup('demon', stage)
         this.player = player
@@ -24,6 +28,17 @@ function Game() {
 
         renderer.render(stage)
         this.gameLoop()
+    }
+
+    this.bumpCollisions = () => {
+        BUMP.contain(
+            this.player.sprite,
+            {x:0, y:0, width: window.innerWidth, height: window.innerHeight},
+            true,
+            hit => {
+                // console.log(hit)
+            }
+        )
     }
 
 }
